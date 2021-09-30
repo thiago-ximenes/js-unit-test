dc2e9f1a54736c6b6d7ef94e0b75ee58d324a307
Thiago Ximenes <thiagoximeneslima@gmail.com>
2021-09-30 19:04:41 -0300

    Finaliza requisito 8 🚀 - Corrige erro de linter

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,7 +16,6 @@
   myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 */
 
-
 const myCounter = () => {
   var myArray = [];
   for (var counter = 0; counter <= 3; counter += 1) {
@@ -27,6 +26,5 @@ const myCounter = () => {
   }
   return myArray;
 };
-
 
 module.exports = myCounter;
